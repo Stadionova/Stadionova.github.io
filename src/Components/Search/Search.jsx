8ceee2970eb969218c04af414626c85a3542c886
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-06-18 16:07:54 +0300

    added setTimeout for search input

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -4,15 +4,29 @@ import React from "react";
 
 const Search = () => {
     const allSnipets = React.createRef();
+    let isTimerStarted = false;
+    let timerId;
 
     function showAllSnipets() {
         allSnipets.current.style.display = 'block';
     }
 
+    function catchInputChanges() {
+        isTimerStarted = true;
+        if (isTimerStarted) {
+            clearTimeout(timerId);
+            timerId = setTimeout(() => {
+                showAllSnipets();
+                isTimerStarted = false;
+                clearTimeout(timerId);
+            }, 1000);
+        }
+    }
+
     return (
         <div className={classes.searchInput}>
             <div className={classes.searchBlock}>
-                <input type="text" />
+                <input type="text" onChange={catchInputChanges} />
                 <button onClick={showAllSnipets}>SEARCH</button>
             </div>
             <Snippets buttonRef={allSnipets} />
