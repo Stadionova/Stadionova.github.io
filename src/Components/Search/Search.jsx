197c3717d9742cb9b76dd9e229156d9c0c0021b7
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-06-21 16:59:08 +0300

    maped data

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -3,44 +3,51 @@ import Snippets from "../Snippets/Snippets";
 import React from "react";
 import axios from "axios";
 
-const Search = () => {
-    const allSnipets = React.createRef();
-    const inputValue = React.createRef();
+const allSnipets = React.createRef();
+const inputValue = React.createRef();
 
-    let isTimerStarted = false;
-    let timerId;
-    let currentInputValue;
+let isTimerStarted = false;
+let timerId;
+let currentInputValue;
 
-    function catchInputChanges() {
+class Search extends React.Component {
+    state = {
+        data: []
+    };
+    getServerData = (currentInputValue) => {
+        axios.get(`https://openlibrary.org/search.json?q=${currentInputValue}`).then(response => {
+            if (response.status === 200 && allSnipets.current) {
+                allSnipets.current.style.display = 'block';
+                this.setState({
+                    data: response.data.docs
+                });
+            }
+        });
+    }
+    catchInputChanges = () => {
         currentInputValue = inputValue.current.value.replace(/ /g, "+").toLowerCase();
         isTimerStarted = true;
         if (isTimerStarted) {
             clearTimeout(timerId);
             timerId = setTimeout(() => {
-                getServerData(currentInputValue);
+                this.getServerData(currentInputValue);
                 isTimerStarted = false;
                 clearTimeout(timerId);
             }, 1000);
         }
     }
-
-    function getServerData(currentInputValue) {
-        axios.get(`https://openlibrary.org/search.json?q=${currentInputValue}`).then(response => {
-            if (response.status === 200 && allSnipets.current) {
-                allSnipets.current.style.display = 'block';
-            }
-        });
-    }
-
-    return (
-        <div className={classes.searchInput}>
-            <div className={classes.searchBlock}>
-                <input type="text" onChange={catchInputChanges} ref={inputValue} placeholder='type title of the book here' />
-                <button onClick={getServerData}>SEARCH</button>
+    render = () => {
+        return (
+            <div className={classes.searchInput}>
+                <div className={classes.searchBlock}>
+                    <input type="text" onChange={this.catchInputChanges}
+                        ref={inputValue} placeholder='type title of the book here' />
+                    <button onClick={this.getServerData}>SEARCH</button>
+                </div>
+                <Snippets buttonRef={allSnipets} data={this.state.data} />
             </div>
-            <Snippets buttonRef={allSnipets} />
-        </div>
-    )
+        )
+    }
 }
 
 export default Search;
--- src/Components/Snippets/Snippets.jsx
+++ src/Components/Snippets/Snippets.jsx
@@ -2,26 +2,28 @@ import classes from "./Snippets.module.scss";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const initialState = {
-    cover: "https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg",
-    title: 'Название книги',
-    author: 'Автор книги'
-}
+const hardcodedBookCover = "https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg";
 
-const Snippets = (props) => {
+const Snippets = ({ data, buttonRef }) => {
     return (
-        <div className={classes.snippetsWrapper} ref={props.buttonRef} >
-            <NavLink
-                to={{
-                    pathname: '/details/' + 1
-                }}>
-                <div className={classes.eachSnippet}>
-                    <img src={initialState.cover} alt="обложка книги" />
-                    <h1>{initialState.title}</h1>
-                    <p>{initialState.author}</p>
-                </div>
-            </NavLink>
-        </div>
+        <div className={classes.snippetsWrapper} ref={buttonRef} >
+            {data.map(book => {
+                const dataKey = book.key.split('/');
+                const id = dataKey[dataKey.length - 1];
+                return (
+                    <NavLink
+                        to={{
+                            pathname: book.key
+                        }}>
+                        <div className={classes.eachSnippet} key={id}>
+                            <img src={hardcodedBookCover} alt="обложка книги" />
+                            <h1>{book.title}</h1>
+                            <p>{book['author_name'] ? book['author_name'][0] : ''}</p>
+                        </div>
+                    </NavLink>
+                )
+            })}
+        </div >
     )
 }
 
