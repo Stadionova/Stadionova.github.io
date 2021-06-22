107a16b3f4c55bedba9e6204ed97ad29a119011f
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-06-22 17:29:52 +0300

    added default bookCover in detailed component

--- src/Components/DetailedSnippet/DetailedSnippet.jsx
+++ src/Components/DetailedSnippet/DetailedSnippet.jsx
@@ -4,10 +4,13 @@ import { NavLink } from "react-router-dom";
 
 const DetailedSnippet = (props) => {
     const bookDetailedData = props.location;
+    function addDefaultSrc(event) {
+        event.target.src = 'https://klike.net/uploads/posts/2020-04/1587719791_1.jpg';
+    }
     return (
         <div className={classes.modalWindow}>
             <div className={classes.detailedSnippetWrapper}>
-                <img src={bookDetailedData.cover} alt="" />
+                <img onError={addDefaultSrc} src={bookDetailedData.cover} alt="" />
                 <h1>НАЗВАНИЕ {bookDetailedData.title}</h1>
                 <p>АВТОР {bookDetailedData.author}</p>
                 <p>ДАТА ПУБЛИКАЦИИ {bookDetailedData.publishDate}</p>
