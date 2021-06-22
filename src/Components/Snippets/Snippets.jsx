3140a26f014c31d1741fcd78e4f52ade3d563ff2
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-06-22 15:06:40 +0300

    passed medium book cover to detailed component

--- src/Components/Snippets/Snippets.jsx
+++ src/Components/Snippets/Snippets.jsx
@@ -2,8 +2,6 @@ import classes from "./Snippets.module.scss";
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const hardcodedBookCover = "https://upload.wikimedia.org/wikipedia/ru/1/10/%D0%9E%D0%B1%D0%BB%D0%BE%D0%B6%D0%BA%D0%B0_%D0%BA%D0%BD%D0%B8%D0%B3%D0%B8_%22%D0%9D%D0%B0%D0%B2%D0%B0%D0%B6%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%22%2C_%D0%9C%D0%B0%D0%BA%D1%81_%D0%A4%D1%80%D0%B0%D0%B9.jpg";
-
 const Snippets = ({ data, buttonRef }) => {
     return (
         <div className={classes.snippetsWrapper} ref={buttonRef} >
@@ -20,7 +18,7 @@ const Snippets = ({ data, buttonRef }) => {
                             publishDate: book['publish_date'] ? book['publish_date'][0] : '',
                             publisher: book.publisher ? book.publisher[0] : '',
                             isbn: book.isbn ? book.isbn[0] : '',
-                            cover: hardcodedBookCover
+                            cover: `http://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`
                         }}>
                         <div className={classes.eachSnippet} key={id}>
                             <img src={`http://covers.openlibrary.org/b/isbn/${isbn}-S.jpg`} alt="обложка книги" />
