c54ce76607e93b6294c6a0e108b0efa6d9e31a7c
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-07-02 21:16:18 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      {/* <Redirect from='/' to='/' />
-      <Redirect from='/works/' to='/' /> */}
+      {/* <Redirect from='/' to='/' /> */}
+      <Redirect from='/works/' to='/' />
       <Route path='/works/' component={DetailedSnippet} />
       <Route path='/' component={SearchContainer} />
     </div>
